ad03ec32e4e7e32a13c2d13ef1ec38f7089e80c1
DaniilCheban <danyacheban@icloud.com>
2023-11-15 21:52:59 +0100

    hw 8,1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const formInput = document.querySelector('.feedback-form');
 
+let clientData = {};
+const inputTextTrottle = throttle(inputText, 500);
+formInput.addEventListener('input', inputTextTrottle);
+
+function inputText(event) {
+  clientData[event.target.name] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(clientData));
+}
+
+function checkLocalStorage() {
+  const userdata = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (userdata !== null) {
+    clientData = userdata;
+    for (const key in userdata) {
+      formInput.elements[key].value = userdata[key];
+    }
+  }
+}
+checkLocalStorage();
+
+formInput.addEventListener('submit', clickOnSubmit);
+function clickOnSubmit(evt) {
+  evt.preventDefault();
+  if (evt.target.email.value === '' || evt.target.message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  console.log(clientData);
+  evt.target.reset();
+  localStorage.removeItem('feedback-form-state');
+  clientData = {};
+}
